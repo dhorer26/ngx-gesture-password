dbc28e722a207742aac0dcf9d06af97eaac2acc6
Ravindra Dhore <ravindrad@ansh.com>
2019-12-25 17:06:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import { ERR } from 'ngx-gesture-password';
+
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  pwd = '1236';
+  type = 'check';
+  switchState = true;
+  Router: any;
+
+  onChecked(data: any) {
+    console.log('onChecked', data);
+    switch (data.err) {
+      case ERR.NOT_ENOUGH_POINTS:
+        console.log('NOT ENOUGH POINTS');
+        break;
+      case ERR.PASSWORD_MISMATCH:
+        console.log('PASSWORD MISMATCH');
+        break;
+      default:
+        console.log('SUCCESS');
+        window.location.href = 'https://stackoverflow.com';
+        break;
+    }
+  }
 }
